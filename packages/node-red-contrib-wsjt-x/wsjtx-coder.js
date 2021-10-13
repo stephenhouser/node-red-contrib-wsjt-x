3b51cc96f737b839519ccba82e94064e03203be5
Stephen Houser <houser@bowdoin.edu>
2021-10-13 15:54:52 -0400

    reworking coder

---
+++ packages/node-red-contrib-wsjt-x/wsjtx-coder.js
@@ -0,0 +1,418 @@
+/* wsjtx-decode.js - NodeRed node for decoding WSJT-X messages
+ *
+ * An out of date reference. 
+ * https://github.com/roelandjansen/wsjt-x/blob/master/NetworkMessage.hpp
+ * Consult the source for more accurate details:
+ * https://www.physics.princeton.edu/pulsar/k1jt/wsjtx.html
+ * QT
+ * https://doc.qt.io/archives/qt-4.8//datastreamformat.html
+ *
+ * 2021/10/11 Stephen Houser, MIT License
+ */
+//const binaryParser = require('binary-parser').Parser;
+const binaryParser = require('binary-parser-encoder').Parser;
+
+const nullParser = new binaryParser();
+
+const stringParser = new binaryParser()
+	.uint32('length', {
+		formatter: function(l) {
+			return l === 0xffffffff ? 0 : l;
+		}
+	})
+	.string('string', { length: 'length' });
+
+function stringFormatter(s) {
+	return s.string.replace(/^\s+|\s+$/g, '');
+}
+
+const dateTimeParser = new binaryParser()
+	.uint64('day')
+	.uint32('time', { formatter: timeFormatter })
+	.uint8('timespec', {
+		formatter: function(t) {
+			switch (t) {
+				case 0: return 'local';
+				case 1: return 'utc';
+				case 2: return 'offset';
+				case 3: return 'timezone';
+			}
+		}
+	})
+	.choice('time_options', {
+		tag: 'timespec',
+		defaultChoice: nullParser,
+		choices: {
+			2: new binaryParser().uint32('offset'),
+		}
+	});
+
+function timeFormatter(t) {
+	var d = new Date();
+	d.setUTCHours(0, 0, 0, 0);
+	d = new Date(d.valueOf() + t);
+	return d.valueOf();
+}
+
+function specialOperationModeFormatter(mode) {
+	const special_operation_mode = [
+		'NONE', 'NA VHF', 'EU VHF', 'FIELD DAY', 'RTTY RU', 'WW DIGI', 'FOX', 'HOUND'
+	];
+	return mode < special_operation_mode.length ? special_operation_mode[mode] : 'unknown';
+}
+
+function boolFormatter(b) {
+	return b === 1;
+}
+
+function numberFormatter(n) {
+	const intermediate = stringFormatter(n);
+	return isNaN(intermediate) ? intermediate : Number(intermediate);
+}
+
+function maxUnit32Formatter(v) {
+	return v === 0xffffffff ? null : v;
+}
+
+// (untested)
+const colorParser = new binaryParser()
+	.uint8('colorspec')
+	.uint16('alpha')
+	.uint16('red')
+	.uint16('green')
+	.uint16('blue')
+	.uint16('pad');
+
+// *** Parsers for WSJT-X Datagrams ***
+
+// Out/In since v2.0
+const heartbeatParser = new binaryParser()
+	.endianess('big')
+	.uint32('max_schema_number')
+	.nest('version', { type: stringParser, formatter: stringFormatter })
+	.nest('revision', { type: stringParser, formatter: stringFormatter });
+
+// Out since v2.0
+const statusParser = new binaryParser()
+	.uint64('freqency')
+	.nest('mode', { type: stringParser, formatter: stringFormatter })
+	.nest('dx_call', { type: stringParser, formatter: stringFormatter })
+	.nest('report', { type: stringParser, formatter: numberFormatter })
+	.nest('tx_mode', { type: stringParser, formatter: stringFormatter })
+	.uint8('tx_enabled', { formatter: boolFormatter })
+	.uint8('transmitting', { formatter: boolFormatter })
+	.uint8('decoding', { formatter: boolFormatter })
+	.uint32('rx_df')
+	.uint32('tx_df')
+	.nest('de_call', { type: stringParser, formatter: stringFormatter })
+	.nest('de_grid', { type: stringParser, formatter: stringFormatter })
+	.nest('dx_grid', { type: stringParser, formatter: stringFormatter })
+	.uint8('tx_watchdog', { formatter: boolFormatter })
+	.nest('sub_mode', { type: stringParser, formatter: stringFormatter })
+	.uint8('fast_mode', { formatter: boolFormatter })
+	.uint8('special_operation_mode', { formatter: specialOperationModeFormatter })
+	// since v2.1
+	.uint32('frequency_tolerance', { formatter: maxUnit32Formatter })
+	.uint32('tr_period', { formatter: maxUnit32Formatter })
+	.nest('configuration_name', { type: stringParser, formatter: stringFormatter })
+	// since v2.3
+	.nest('tx_message', { type: stringParser, formatter: stringFormatter });
+
+// Out since v2.0
+const decodeParser = new binaryParser()
+	.endianess('big')
+	.uint8('new', { formatter: boolFormatter })
+	.uint32('time', { formatter: timeFormatter })
+	.int32('snr')
+	.doublebe('delta_time')
+	.uint32('delta_frequency')
+	.nest('mode', { type: stringParser, formatter: stringFormatter })
+	.nest('message', { type: stringParser, formatter: stringFormatter })
+	.uint8('low_confidence')
+	.uint8('off_air');
+
+// Out/In since v2.0
+const clearParser = new binaryParser()
+	.endianess('big')
+	// since v2.1
+	.uint8('window');
+
+function clearEncoder(message) {
+
+}
+
+// function replyModifierFormatter(modifier) {
+// 	const _modifier = [
+// 		'NONE', 'SHIFT', 'CTRL', 'ALT', 'WINDOWS', 'KEYPAD', 'GROUP'
+// 	];
+// 	return 'unknown';
+// }
+
+const modifier_type = {
+	none: 0x00,
+	shift: 0x01,
+	ctrl: 0x02,
+	alt: 0x04,
+	windows: 0x10,
+	keypad: 0x20,
+	group: 0x40,
+
+	format: function(modifiers) {
+
+	},
+	encode: function(modifiers) {
+
+	}
+}
+
+// In  (untested) since v2.0
+const replyParser = new binaryParser()
+	.endianess('big')
+	.uint32('time', { formatter: timeFormatter })
+	.int32('snr')
+	.doublebe('delta_time')
+	.uint32('delta_frequency')
+	.nest('mode', { type: stringParser, formatter: stringFormatter })
+	.nest('message', { type: stringParser, formatter: stringFormatter })
+	.uint8('low_confidence')
+	.uint8('modifiers');
+/* Modifiers
+ * 0x00 - none
+ * 0x02 - shift
+ * 0x04 - ctrl or CMD
+ * 0x08 - ALT
+ * 0x10 - windows key on windows
+ * 0x20 - keypad or arrows
+ * 0x40 - group switch X11 only
+ */
+
+function replyEncoder(message) {
+
+}
+
+// Out since v2.0
+const qsoLoggedParser = new binaryParser()
+	.nest('date_time_on', { type: dateTimeParser })
+	.nest('dx_call', { type: stringParser, formatter: stringFormatter })
+	.nest('dx_grid', { type: stringParser, formatter: stringFormatter })
+	.uint64('tx_frequency')
+	.nest('mode', { type: stringParser, formatter: stringFormatter })
+	.nest('report_sent', { type: stringParser, formatter: numberFormatter })
+	.nest('report_received', { type: stringParser, formatter: numberFormatter })
+	.nest('tx_power', { type: stringParser, formatter: numberFormatter })
+	.nest('comments', { type: stringParser, formatter: stringFormatter })
+	.nest('name', { type: stringParser, formatter: stringFormatter })
+	.nest('date_time_off', { type: dateTimeParser })
+	.nest('operator_call', { type: stringParser, formatter: stringFormatter })
+	.nest('my_call', { type: stringParser, formatter: stringFormatter })
+	.nest('my_grid', { type: stringParser, formatter: stringFormatter })
+	.nest('exchange_sent', { type: stringParser, formatter: stringFormatter })
+	.nest('exchange_received', { type: stringParser, formatter: stringFormatter })
+	// since 2.3
+	.nest('adif_propogation_mode', { type: stringParser, formatter: stringFormatter });
+
+// Out (untested) since v2.0
+const closeParser = new binaryParser();
+
+// In (untested) since v2.0
+const replayParser = new binaryParser();
+
+// In (untested) since v2.0
+const haltTxParser = new binaryParser()
+	.endianess('big')
+	.uint8('auto_tx_only', { formatter: boolFormatter });
+
+// In (untested) since v2.0
+const freeTextParser = new binaryParser()
+	.endianess('big')
+	.nest('text', { type: stringParser, formatter: stringFormatter })
+	.uint8('send', { formatter: boolFormatter });
+
+// Out (untested) since v2.0
+const wsprDecodeParser = new binaryParser()
+	.endianess('big')
+	.uint8('new', { formatter: boolFormatter })
+	.uint32('time', { formatter: timeFormatter })
+	.int32('snr')
+	.doublebe('delta_time')
+	.uint64('frequency')
+	.uint32('drift')
+	.nest('callsign', { type: stringParser, formatter: stringFormatter })
+	.nest('grid', { type: stringParser, formatter: stringFormatter })
+	.uint32('power')
+	.uint8('off_air');
+
+// In (untested) since v2.0
+const locationParser = new binaryParser()
+	.endianess('big')
+	.nest('location', { type: stringParser, formatter: stringFormatter });
+
+// Out since v2.0
+const loggedAdifParser = new binaryParser()
+	.endianess('big')
+	.nest('adif_text', { type: stringParser, formatter: stringFormatter });
+
+// In (untested) since v2.0
+const highlightCallsignParser = new binaryParser()
+	.endianess('big')
+	.nest('background_color', { type: colorParser })
+	.nest('foreground_color', { type: colorParser })
+	.uint8('highlight_last', { formatter: boolFormatter });
+
+// In (untested) since v2.1
+const swicthConfigurationParser = new binaryParser()
+	.endianess('big')
+	.nest('configuration_name', { type: stringParser, formatter: stringFormatter });
+
+// In (untested) since v2.1
+const configureParser = new binaryParser()
+	.endianess('big')
+	.nest('mode', { type: stringParser, formatter: stringFormatter })
+	.uint32('frequency_tolerance', { formatter: maxUnit32Formatter })
+	.nest('sub_mode', { type: stringParser, formatter: stringFormatter })
+	.uint8('fast_mode', { formatter: boolFormatter })
+	.uint32('tr_period', { formatter: maxUnit32Formatter })
+	.uint32('rx_df')
+	.nest('dx_call', { type: stringParser, formatter: stringFormatter })
+	.nest('dx_grid', { type: stringParser, formatter: stringFormatter })
+	.uint8('generate_message', { formatter: boolFormatter });
+
+// *** The root parser ***
+
+const message_type = {
+	heartbeat: 0,
+	status: 1,
+	decode: 2,
+	clear: 3,
+	reply: 4,
+	qso_logged: 5,
+	close: 6,
+	replay: 7,
+	halt_tx: 8,
+	free_text: 9,
+	wspr_decode: 10,
+	location: 11,
+	logged_adif: 12,
+	highlight_callsign: 13,
+	switch_configuration: 14,
+	configure: 15,
+
+	_names: [
+		'heartbeat', 'status', 'decode', 'clear', 'reply', 'qso-logged',
+		'close', 'replay', 'halt-tx', 'free-text', 'wspr-decode', 'location',
+		'logged-adif', 'highlight-callsign', 'switch-configuration', 'configure'
+	],
+
+	format: function(code) {
+		if (code < this._names.length) {
+			return this._names[code]
+		}
+
+		return 'unknown';
+	},
+
+	encode: function(key) {
+		return this._names.indexOf(key);
+	}
+};
+
+const wsjtxParser = new binaryParser()
+	.endianess('big')
+	.uint32('magic')
+	.uint32('version')
+	.uint32('type')
+	.nest('id', { type: stringParser, formatter: stringFormatter })
+	.choice(null, {
+		tag: 'type',
+		defaultChoice: nullParser,
+		choices: {
+			0: heartbeatParser,
+			1: statusParser,
+			2: decodeParser,
+			3: clearParser,
+			4: replyParser,
+			5: qsoLoggedParser,
+			6: closeParser,
+			7: replayParser,
+			8: haltTxParser,
+			9: freeTextParser,
+			10: wsprDecodeParser,
+			11: locationParser,
+			12: loggedAdifParser,
+			13: highlightCallsignParser,
+			14: swicthConfigurationParser,
+			15: configureParser
+		}
+	});
+
+function decode_exchange(message) {
+	const exchange = message.split(' ');
+	if (exchange[0] === 'CQ') {
+		if (exchange.length == 4) {
+			return { type: 'cq', dx_call: exchange[1], de_call: exchange[2], de_grid: exchange[3] };
+		} else {
+			return { type: 'cq', de_call: exchange[1], de_grid: exchange[2] };
+		}
+	}
+
+	if (exchange.length <= 2) {
+		return { type: 'tx?', dx_call: exchange[0], de_call: exchange[1] };
+	}
+
+	if (exchange[2] === '73') {
+		return { type: 'tx5', dx_call: exchange[0], de_call: exchange[1], message: exchange[2] };
+	}
+
+	if (exchange[2] === 'RR73' || exchange[2] === 'RRR') {
+		return { type: 'tx4', dx_call: exchange[0], de_call: exchange[1], message: exchange[2] };
+	}
+
+	if (exchange[2].startsWith('+') || exchange[2].startsWith('-')) {
+		return { type: 'tx2', dx_call: exchange[0], de_call: exchange[1], snr: parseInt(exchange[2]) };
+	}
+
+	if (exchange[2].startsWith('R')) {
+		return { type: 'tx3', dx_call: exchange[0], de_call: exchange[1], snr: parseInt(exchange[2].slice(1)) };
+	}
+
+	return { type: 'tx1', dx_call: exchange[0], de_call: exchange[1], de_grid: exchange[2] };
+}
+
+function decode(buffer) {
+	try {
+		const decoded = wsjtxParser.parse(buffer);
+		decoded.type = message_type.format(decoded.type);
+
+		if (decoded.hasOwnProperty('message')) {
+			const message_decode = decode_exchange(decoded.message);
+			decoded.message_decode = message_decode;
+		}
+
+		return decoded;
+	} catch (error) {
+		console.error(error);
+		return null;
+	}
+}
+
+function encode(obj) {
+	const type_code = message_type.indexOf(obj.type);
+	if (type_code < 0) {
+		return null;
+	}
+
+	try {
+		// TODO: encode...
+		console.error('wsjtx.encode() not implemented.');
+		return null;
+
+	} catch (error) {
+		console.error(error);
+		return null;
+	}
+}
+
+module.exports = {
+	decode: decode,
+	encode: encode
+};
---
+++ packages/node-red-contrib-wsjt-x/wsjtx-decode.js
@@ -0,0 +1,39 @@
+/* wsjtx-decode.js - NodeRed node for decoding WSJT-X messages
+ *
+ * An out of date reference. 
+ * https://github.com/roelandjansen/wsjt-x/blob/master/NetworkMessage.hpp
+ * Consult the source for more accurate details:
+ * https://www.physics.princeton.edu/pulsar/k1jt/wsjtx.html
+ *
+ * 2021/10/11 Stephen Houser, MIT License
+ */
+const wsjtx = require('./wsjtx-coder');
+
+module.exports = function(RED) {
+	'use strict';
+
+	function WSJTXDecodeNode(config) {
+		RED.nodes.createNode(this, config);
+
+		const node = this;
+		node.name = config.name;
+
+		node.on('input', function(msg, send, done) {
+			const decoded = wsjtx.decode(msg.payload);
+			if (decoded && send) {
+				const message = {
+					topic: decoded.type,
+					payload: decoded
+				};
+
+				send(message);
+			}
+			
+			if (done) {
+				done();
+			}
+		});
+	}
+
+	RED.nodes.registerType('wsjtx-decode', WSJTXDecodeNode);
+};
